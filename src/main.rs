b6e8a65bdb4f64fc2f15f43326d489cdf6770b02
jamestheengineer <jamestheengineer@gmail.com>
2024-05-17 19:05:25 -0400

    Run fmt

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,11 @@ async fn greet(req: HttpRequest) -> impl Responder {
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     HttpServer::new(|| {
-        App::new().route("/", web::get().to(greet))
-        .route("/{name}", web::get().to(greet))})
-        .bind("127.0.0.1:8000")?.run().await
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
