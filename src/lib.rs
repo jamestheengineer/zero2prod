1cf95aa1ac79b1e084f67c64b477d7dad5610775
jamestheengineer <jamestheengineer@gmail.com>
2024-06-02 11:04:04 -0400

    Let's pass all things on github

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,12 @@ async fn health_check() -> HttpResponse {
 
 #[derive(serde::Deserialize)]
 struct FormData {
-    email: String,
-    name: String,
+    _email: String,
+    _name: String,
 }
 
 async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+
     HttpResponse::Ok().finish()
 }
 
