10b9126c7e7c74a7ce7c07c6d456579df9c533fa
jamestheengineer <jamestheengineer@gmail.com>
2024-06-04 17:45:24 -0400

    Touch up postgres script. Amazing that just worked.

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -18,5 +18,10 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
 # Launch postgres using Docker
-docker run -e POSTGRES_USER=${DB_USER} -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-  -e POSTGRES_DB=${DB_NAME} -p "${DB_PORT}":5432 -d postgres postgres -N 1000
+docker run \
+  -e POSTGRES_USER=${DB_USER} \
+  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+  -e POSTGRES_DB=${DB_NAME} \
+  -p "${DB_PORT}":5432 \
+  -d postgres \
+  postgres -N 1000
