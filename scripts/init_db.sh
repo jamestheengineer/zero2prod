c5fcc51dfaaad5d70be894dd03a5a1cebf52094a
jamestheengineer <jamestheengineer@gmail.com>
2024-06-06 19:16:36 -0400

    Get a real migration going

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,6 +2,19 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(command -v psql)" ]; then
+  echo >&2 "Error: psql is not installed."
+  exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx is not installed."
+  echo >&2 "Use:"
+  echo >&2 "    cargo install --version='~0.7' sqlx-cli --no-default-features --features rustls,postgres"
+  echo >&2 "to install it."
+  exit 1
+fi
+
 # Check is a customer user has been set, otherwise default to 'postgres'
 DB_USER="${POSTGRES_USER:=postgres}"
 
@@ -25,3 +38,16 @@ docker run \
   -p "${DB_PORT}":5432 \
   -d postgres \
   postgres -N 1000
+
+# Keep pinging Postgres until it's ready to accept commands
+until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
+
+
+  DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+  export DATABASE_URL
+  sqlx database create
