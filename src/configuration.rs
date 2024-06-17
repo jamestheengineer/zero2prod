0a04cc1f6717996e9a5c85a3f987be1baf20a684
jamestheengineer <jamestheengineer@gmail.com>
2024-06-17 19:30:21 -0400

    Connection string method

--- src/configuration.rs
+++ src/configuration.rs
@@ -10,6 +10,7 @@ pub struct DatabaseSettings {
     pub username: String,
     pub password: String,
     pub port: u16,
+    pub host: String,
     pub database_name: String,
 }
 
@@ -24,4 +25,13 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
 
     // Try to convert the confguration values it read into our Settings type
     settings.try_deserialize::<Settings>()
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
 }
