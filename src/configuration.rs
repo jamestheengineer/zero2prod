eee9d990d1e310068594d6de56240e5c0f3e6545
jamestheengineer <jamestheengineer@gmail.com>
2024-11-20 18:43:14 -0500

    new crate for stuff

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,6 +1,7 @@
 //! src/configuration.rs
 use secrecy::ExposeSecret;
 use secrecy::Secret;
+use serde_aux::field_attributes::deserialize_number_from_string;
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -10,6 +11,7 @@ pub struct Settings {
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
@@ -18,6 +20,7 @@ pub struct ApplicationSettings {
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
