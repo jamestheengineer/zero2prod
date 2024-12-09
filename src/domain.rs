caf792c423ee19bc1ccaab63225027f7d941b8b7
jamestheengineer <jamestheengineer@gmail.com>
2024-12-09 18:13:16 -0500

    add some unit tests

--- src/domain.rs
+++ src/domain.rs
@@ -4,6 +4,7 @@ pub struct NewSubscriber {
     pub email: String,
     pub name: SubscriberName,
 }
+#[derive(Debug)]
 pub struct SubscriberName(String);
 
 impl AsRef<str> for SubscriberName {
@@ -19,9 +20,27 @@ impl SubscriberName {
         let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
         let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
         if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
-            panic!("{} is not a valid subscriber name.", s)
+            Err(format!("{} is not a valid subscriber name.", s))
         } else {
             Ok(Self(s))
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberName;
+    use claims::{assert_err, assert_ok};
+
+    #[test]
+    fn a_256_grapheme_long_name_is_valid() {
+        let name = "ë".repeat(256);
+        assert_ok!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn a_name_longer_than_256_graphemes_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::parse(name));
     }
 }
