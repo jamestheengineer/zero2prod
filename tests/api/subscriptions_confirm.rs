cf0515012ae464347f2c2943cc43ddc175188f2a
jamestheengineer <jamestheengineer@gmail.com>
2025-01-20 19:22:36 -0500

    start a new endpoint to verify

---
+++ tests/api/subscriptions_confirm.rs
@@ -0,0 +1,13 @@
+//! tests/api/subbscriptions_confirm.rs
+use crate::helpers::spawn_app;
+
+#[tokio::test]
+async fn confirmations_without_tocken_are_rejected_with_a_400() {
+    let app = spawn_app().await;
+
+    let response = reqwest::get(&format!("{}/subscriptions/confirm", app.address))
+        .await
+        .unwrap();
+
+    assert_eq!(response.status().as_u16(), 400);
+}
